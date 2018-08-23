d79cb10e4dd4011f22b34336da5ec2c3840f8610
dhf@acewill.cn <dhf@acewill.cn>
2018-08-23 18:51:43 +0800

    功能：常用filter封装

---
+++ src/filter/index.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+Vue.filter("highlight",function(orignal:string){
+    const htmlRegex = /<(.*)>.*|<(.*) \/>/ig;
+    if(htmlRegex.test(orignal)){
+        return orignal;
+    }
+    return template(orignal);
+    function template(key:string) { 
+        return "<span style='color:#11b7f3;'>" + key + "</span>"; 
+    }
+});
+Vue.filter("maxlength",function(value:string,length = 5){
+    let tempItem = value||"";
+    if(tempItem.length>length){
+        tempItem = tempItem.substr(0,length)+"...";
+    }
+    return tempItem;
+});
+Vue.filter("json", (value:any)=> {
+    // 返回处理后的值
+    if(value&&value.split) {
+        return value;
+    }
+    return JSON.stringify(value||{});
+});
+
+Vue.filter("date", (value:any,format='yyyy-MM-dd hh:mm:ss')=> {
+    // 返回处理后的值
+    return new Date(value).format(format);
+});
+
+Vue.filter("isnull", (value:string)=> {
+    // 返回处理后的值
+    return value||"暂无";
+});

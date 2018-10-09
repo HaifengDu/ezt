90665ef2905739ab9411748ddb74fbce0a6610b3
wanghui <1084836584@qq.com>
2018-10-09 15:32:23 +0800

    盘库查询结果页面

--- src/page/index/stockTaking/QueryResult.vue
+++ src/page/index/stockTaking/QueryResult.vue
@@ -86,24 +86,27 @@ export default class stockTaking extends Vue{
 
     // 盘库详情
     private see(item:any,types:any,audit_status:number){
-      this.service.getLibraryDetails(item.id,audit_status).then(res=>{ 
-        this.$router.push({
+      this.$router.push({
           name:'LibraryDetails',
-          query:{
-              warehouse_name:item.warehouse_name,
-              warehouse_id:item.warehouse_id,
-              busi_date:item.busi_date,
-              bill_type_name:item.bill_type_name,
-              stock_count_mode_name:item.stock_count_mode_name,
-              ids:item.id,  
-              types:types,
-              stock_count_mode:item.stock_count_mode,
-              // template_name:this.inventoryDetails.template_name,
-            }});  
-          // this.setInventoryDetails(res.data.data); 
-      },err=>{
-          this.$toasted.show(err.message)
-      })
+          });  
+      // this.service.getLibraryDetails(item.id,audit_status).then(res=>{ 
+      //   this.$router.push({
+      //     name:'LibraryDetails',
+      //     query:{
+      //         warehouse_name:item.warehouse_name,
+      //         warehouse_id:item.warehouse_id,
+      //         busi_date:item.busi_date,
+      //         bill_type_name:item.bill_type_name,
+      //         stock_count_mode_name:item.stock_count_mode_name,
+      //         ids:item.id,  
+      //         types:types,
+      //         stock_count_mode:item.stock_count_mode,
+      //         // template_name:this.inventoryDetails.template_name,
+      //       }});  
+      //     // this.setInventoryDetails(res.data.data); 
+      // },err=>{
+      //     this.$toasted.show(err.message)
+      // })
     } 
     
 

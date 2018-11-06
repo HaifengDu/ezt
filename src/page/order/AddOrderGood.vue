0b5f6edcda52eaaf76bff4245171f2c50c764601
Tnmary <826869317@qq.com>
2018-11-06 17:17:06 +0800

    新增删除物料 图标

--- src/page/order/AddOrderGood.vue
+++ src/page/order/AddOrderGood.vue
@@ -116,7 +116,9 @@
                                 </div>                 
                             </div>
                         </div>
-                        <div class="ezt-detail-del" @click="delAction(item)">删除</div> 
+                        <div class="ezt-detail-del" @click="delAction(item)">
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+                        </div> 
                     </li>
                 </ul>   
             </div>
@@ -721,7 +723,6 @@ export default class Order extends Vue{
     position: absolute;
     right: 10px;
     top: 30px;
-    background: pink;
     width: 50px;
     height: 50px;
     text-align: center;
